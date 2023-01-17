10dafb285db90a541ebcb8b11b5b3dcbd14ae9fb
Samuel Meľuch <samuelmeluch@gmail.com>
2023-01-17 17:07:46 +0100

    datepicker

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,25 +1,58 @@
 import { modifyMeasure } from "@gooddata/sdk-model";
 import { LineChart } from "@gooddata/sdk-ui-charts";
-import React from "react";
+import { DateFilterHelpers, DateFilterOption, defaultDateFilterOptions } from "@gooddata/sdk-ui-filters";
+import React, { useEffect, useMemo, useState } from "react";
 
 import Page from "../../components/Page";
 import * as Md from "../../md/full";
-import DashboardDatePicker from "./components/DashboardDatePicker";
+import { DashboardDatePicker } from "./components/DashboardDatePicker";
 
 const Revenue = modifyMeasure(Md.Revenue, (m) => m.format("#,##0"));
 const Product = Md.Product.Default;
 
 const style = { height: 600 };
 
+export interface IDateFilterComponentState {
+    selectedFilterOption: DateFilterOption;
+    excludeCurrentPeriod: boolean;
+}
+
 const Dashboard: React.FC = () => {
+    const [dateState, setDateState] = useState<IDateFilterComponentState>({
+        selectedFilterOption: defaultDateFilterOptions.allTime!,
+        excludeCurrentPeriod: false,
+    });
+
+    const dateFilter = useMemo(() => {
+        return DateFilterHelpers.mapOptionToAfm(
+            dateState.selectedFilterOption,
+            Md.DateDatasets.Date.ref,
+            dateState.excludeCurrentPeriod,
+        );
+    }, [dateState]);
+
+    useEffect(() => {
+        console.log(dateFilter);
+    }, [dateFilter]);
+
+    const onDatePickerApply = (options: IDateFilterComponentState) => {
+        setDateState({ ...options });
+        console.log(options);
+    };
+
     return (
         <Page>
-            <DashboardDatePicker />
+            <DashboardDatePicker
+                selectedFilterOption={dateState.selectedFilterOption}
+                excludeCurrentPeriod={dateState.excludeCurrentPeriod}
+                onDatePickerApply={onDatePickerApply}
+            />
             <div style={style} className="s-line-chart">
                 <LineChart
                     measures={[Revenue]}
-                    trendBy={Md.DateDatasets.Date.Month.Short}
+                    trendBy={Md.DateDatasets.Date.Month.Long}
                     segmentBy={Product}
+                    filters={dateFilter ? [dateFilter] : []}
                 />
             </div>
         </Page>
--- src/pages/dashboard/components/DashboardDatePicker.tsx
+++ src/pages/dashboard/components/DashboardDatePicker.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { DateFilter, DateFilterOption, defaultDateFilterOptions } from "@gooddata/sdk-ui-filters";
+import { DateFilterGranularity } from "@gooddata/sdk-model";
+import { IDateFilterComponentState } from "../Dashboard";
 
-const DashboardDatePicker: React.FC = () => {
-    return <div>date picker</div>;
+const availableGranularities: DateFilterGranularity[] = [
+    "GDC.time.date",
+    "GDC.time.month",
+    "GDC.time.quarter",
+    "GDC.time.year",
+];
+
+const style = { width: 300 };
+
+export interface IDatePickerProps extends IDateFilterComponentState {
+    onDatePickerApply: (options: IDateFilterComponentState) => void;
+}
+
+export const DashboardDatePicker: React.FC<React.PropsWithChildren<IDatePickerProps>> = ({
+    selectedFilterOption,
+    excludeCurrentPeriod,
+    onDatePickerApply,
+}) => {
+    const onApply = (selectedFilterOption: DateFilterOption, excludeCurrentPeriod: boolean) => {
+        onDatePickerApply({ selectedFilterOption, excludeCurrentPeriod });
+    };
+
+    return (
+        <div style={style}>
+            <DateFilter
+                excludeCurrentPeriod={excludeCurrentPeriod}
+                selectedFilterOption={selectedFilterOption}
+                filterOptions={defaultDateFilterOptions}
+                availableGranularities={availableGranularities}
+                customFilterName="Selected date"
+                dateFilterMode="active"
+                dateFormat="MM/dd/yyyy"
+                onApply={onApply}
+            />
+        </div>
+    );
 };
-
-export default DashboardDatePicker;
