39b6aa88421b7439b2dc0ab504206e80d78c0bdb
Samuel Meľuch <samuelmeluch@gmail.com>
2023-01-17 14:38:32 +0100

    basic product/revenue chart test

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,23 @@
+import { modifyMeasure } from "@gooddata/sdk-model";
+import { BarChart } from "@gooddata/sdk-ui-charts";
 import React from "react";
 
 import Page from "../components/Page";
+import * as Md from "../md/full";
+
+const Revenue = modifyMeasure(Md.Revenue, (m) => m.format("#,##0"));
+const Product = Md.Product.Default;
+
+const style = { height: 300 };
 
 const Home: React.FC = () => {
-    return <Page>Place your content here&hellip;</Page>;
+    return (
+        <Page>
+            <div style={style} className="s-bar-chart">
+                <BarChart measures={[Revenue, Product]} />
+            </div>
+        </Page>
+    );
 };
 
 export default Home;
